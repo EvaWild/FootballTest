349a45a4f4254c512d619ce11415e8de6207a42d
Anton Lapshin <antvallap@gmail.com>
2016-03-23 18:08:06 +0200

    Added photos

--- javascripts/vk.js
+++ javascripts/vk.js
@@ -1,8 +1,9 @@
-var photo_id = 'photo-71627954_402632215';
+var photo1 = 'photo-71627954_404741670';
+var photo2 = 'photo-71627954_404741553';
+var photo3 = 'photo-71627954_404741553';
 
 VK.init(function() { 
   	console.log('Successful initializtion');
-  	post();
   	//VK.callMethod('showInviteBox');
      // API initialization succeeded 
      // Your code here 
@@ -12,8 +13,15 @@ VK.init(function() {
      // Can reload page here 
 }, '5.50');
 
-function post(){
-	VK.api('wall.post', { message: 'Hello', attachments: photo_id, from_group: 1 }, function(result){
+function post(score){
+	var photo;
+	if (score <= 4)
+		photo = photo1;
+	else if (sore <=9)
+		photo = photo2;
+	else 
+		photo = photo3;
+	VK.api('wall.post', { message: 'Hello', attachments: photo, from_group: 1 }, function(result){
 		console.log('wall.post: ' + result);
 	});
 }
